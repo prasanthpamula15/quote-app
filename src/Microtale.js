b72e0cf40aa082c6b356531279e442e2d173e221
prasanthpamula15 <prasanthpamula583@gmail.com>
2021-11-11 21:08:09 +0530

    changes

--- src/Microtale.js
+++ src/Microtale.js
@@ -8,19 +8,73 @@ export default class Microtale extends Component {
         return (
           <div id="m-grid"> 
                 <div id="grid">
+                
                
-                  <div className="grid_item">
-                   <div ><img className="card" ></img></div>
-                      <div className="details">
-                      <span class="font"> <BsHeart size="25px" /></span>
-                      <span class="font"> <FaShareAlt size="25px" /></span>
-                      <span class="font"> <FaDownload size="25px" /></span>
-                      
-                   
-                      </div>
+               <div className="grid_item">
+                   <div ><img className="card" alt="logo" src={image.logo1}></img></div>
+                   <div className="details">
+                   <img src={logo} alt="logo" className="card-acc" /><p className="acc-name">Keerthana_Suresh</p>
+                   <div class="download_button"> <a className="font" href={image.logo2} download> <FaDownload size="30px" color="black"/></a></div>
+                   </div>
+               
                </div>
-               <div class="grid_item">2</div>
-               <div class="grid_item">3</div>
+               <div className="grid_item">
+                   <div ><img className="card" alt="logo" src={image.logo2}></img></div>
+                   <div className="details">
+                   <img src={logo} alt="logo" className="card-acc" /><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo3} download> <FaDownload size="30px" color="black" /></a>
+                   </div>
+                  
+               
+               </div>
+               <div className="grid_item">
+                   <div ><img className="card" alt="logo" src={image.logo3}></img></div>
+                   <div className="details">
+                   <img src={logo} logo="none" alt="logo" className="card-acc" /><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo4} download> <FaDownload size="30px" color="black" className="download"/></a>
+                   </div>
+               
+               </div>
+               <div className="grid_item">
+                   <div ><img className="card"  alt="none" src={image.logo4}></img></div>
+                   <div className="details">
+                   <img src={logo} className="card-acc" alt="logo"/><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo5} alt="logo" download> <FaDownload size="30px" color="black" className="download"/></a>
+                   </div>
+               
+               </div>
+               <div className="grid_item">
+                   <div ><img className="card" src={image.logo5} alt="logo"></img></div>
+                   <div className="details">
+                   <img src={logo} className="card-acc" alt="logo" /><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo7} download> <FaDownload size="30px" color="black" className="download"/></a>
+                   </div>
+               
+               </div>
+               <div className="grid_item">
+                   <div ><img className="card" src={image.logo7} alt="logo"></img></div>
+                   <div className="details">
+                   <img src={logo} className="card-acc" alt="logo"/><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo7} download> <FaDownload size="30px" color="black" className="download"/></a>
+                   </div>
+               
+               </div>
+               <div className="grid_item">
+                   <div ><img className="card" src={image.logo8} alt="logo"></img></div>
+                   <div className="details">
+                   <img src={logo} className="card-acc" alt="logo"/><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo8} download> <FaDownload size="30px" color="black" className="download"/></a>
+                   </div>
+               
+               </div>
+               <div className="grid_item">
+                   <div ><img className="card" src={image.logo9} alt="logo"></img></div>
+                   <div className="details">
+                   <img src={logo} className="card-acc" alt="logo"/><p className="acc-name">Keerthana_Suresh</p>
+                   <a class="download_button" href={image.logo9} download> <FaDownload size="30px" color="black" className="download"/></a>
+                   </div>
+               
+               </div>
             </div>
             </div>
         )
---
+++ src/m-images.js
@@ -0,0 +1,13 @@
+
+
+ const images={
+    logo1: "./m/m (1).webp",
+    logo2: "./m/m (2).webp",
+    logo3: "./m/m (3).webp",
+    logo4: "./m/m (4).webp",
+    logo5: "./m/m (5).webp",
+    logo7: "./m/m (7).webp",
+    logo8: "./m/m (8).webp",
+    logo9: "./m/m (9).webp",
+ }
+ export default images;
